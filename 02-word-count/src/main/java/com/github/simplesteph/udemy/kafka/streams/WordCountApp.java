4e8140fe549c3ec3fba52b5052240054493c9a6e
Alfonso Tienda <atienda@iprocuratio.com>
2020-06-22 10:10:15 +0200

    streams1

---
+++ 02-word-count/src/main/java/com/github/simplesteph/udemy/kafka/streams/WordCountApp.java
@@ -0,0 +1,63 @@
+package com.github.simplesteph.udemy.kafka.streams;
+
+import java.util.Properties;
+import java.util.Arrays;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KStreamBuilder;
+import org.apache.kafka.streams.kstream.KTable;
+
+public class WordCountApp {
+    public static void main(String[] args) {
+        Properties config = new Properties();
+        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount-application");
+        config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        KStreamBuilder builder = new KStreamBuilder();
+        // 1 - stream de Kafka
+
+        KStream<String, String> textLines = builder.stream("word-count-input");
+        KTable<String, Long> wordCounts = textLines
+                // 2 - pasar los valores a lowercase
+                .mapValues(textLine -> textLine.toLowerCase())
+                // 3 - flatmap values split by space
+                .flatMapValues(textLine -> Arrays.asList(textLine.split("\\W+")))
+                // 4 - select key to apply a key (we discard the old key)
+                .selectKey((key, word) -> word)
+                // 5 - agrupar por la key antes de agregar
+                .groupByKey()
+                // 6 - contar ocurrencias
+                .count("Counts");
+
+        // 7 - to in order to write the results back to kafka
+        wordCounts.to(Serdes.String(), Serdes.Long(), "word-count-output");
+
+        KafkaStreams streams = new KafkaStreams(builder, config);
+        streams.start();
+
+
+
+        // shutdown hook to correctly close the streams application
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+
+        // Update:
+        // print the topology every 10 seconds for learning purposes
+        while(true){
+            System.out.println(streams.toString());
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
+
+
+    }
+}
--- README.md
+++ README.md
@@ -1,21 +1,9 @@
-[![Build Status](https://travis-ci.org/simplesteph/kafka-streams-course.svg?branch=master)](https://travis-ci.org/simplesteph/kafka-streams-course)
+Ejemplos para curso kafka - streams
 
-# Learning
+# Contenido
 
-This is a companion repository for my [**Kafka Streams course on Udemy**](https://links.datacumulus.com/kafka-streams-coupon)
-
-Happy learning!
-
-<p align="center">
-    <a href="https://www.udemy.com/kafka-streams/?couponCode=GITHUB">
-        <img src="http://i.imgur.com/YRJijb0.png" alt="Kafka Streams Course Logo"/>
-    </a>
-</p>
-
-# Content
-
- - Starter project with dependencies
- - Word Count to learn the basic API
- - Favourite Colour for a more advanced example (`Scala` version included)
- - Bank Balance to demonstrate exactly once semantics
- - User Event matcher to learn about joins between `KStream` and `GlobalKTable`.
+ - Starter project con dependencias (`Hola mundo`)
+ - Word Count para aprender la API básica
+ - Color favorito para un ejemplo más avanzado 
+ - Bank Balance para demostrar exactly once
+ - User Event matcher para los joins entre `KStream` y `GlobalKTable`.
